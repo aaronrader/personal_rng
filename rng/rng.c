3cb6c1f560ea1ec0b4a3b8358ed60b568ab8a8cd
aaronrader <aaronrader1867+github@gmail.com>
2022-02-08 23:20:58 -0500

    Removed prompts - 1-arg call now simply outputs 1000 random numbers between 0 and 1. File output moved to 5th argument.

--- rng/rng.c
+++ rng/rng.c
@@ -26,10 +26,10 @@ void print_error_message() {
 /*
 * Command line arguments are as follows:
 * 1. rng.exe
-* 2. file to output to				(optional)
-* 3. (unsigned)numbers to generate	(optional)
-* 4. minimum number to generate		(optional)
-* 5. maximum number to generate		(optional)
+* 2. (unsigned)numbers to generate	(optional)
+* 3. minimum number to generate		(optional)
+* 4. maximum number to generate		(optional)
+* 5. file to output to				(optionsl)
 */
 int main(int argc, char* argv[]) {
 	if (argc > 5) {
@@ -43,74 +43,50 @@ int main(int argc, char* argv[]) {
 	double min = 0;
 	FILE* stream = stdout;
 
-	if (argc > 1) { //has command-line arguments (file to write to)
-		stream = fopen(argv[1], "w");
-		if (stream == NULL) {
-			printf("Error: cannot open \"%s\": ", argv[1]);
+	//Numeric argument defined
+	if (argc > 1) {
+		if ((count = strtoull(argv[1], NULL, 0)) == 0 || count == ULONG_MAX) {
+			printf("Error: invalid numeric argument (%s)\n", argv[2]);
 			print_error_message();
 			return EXIT_FAILURE;
 		}
 
-		//Numeric argument defined
-		if (argc > 2) {
-			if ((count = strtoull(argv[2], NULL, 0)) == 0 || count == ULONG_MAX) {
-				printf("Error: invalid numeric argument (%s)\n", argv[2]);
+		//3-arg call, "count" numbers generated - [0,max]
+		if (argc == 3) {
+			if ((max = strtod(argv[2], NULL)) == 0 || max == ULONG_MAX) {
+				printf("Error: invalid numeric argument (%s)\n", argv[3]);
 				print_error_message();
 				return EXIT_FAILURE;
 			}
+		}
 
-			//4-arg call, "count" numbers generated - [0,max]
-			if (argc == 4) {
-				if ((max = strtod(argv[3], NULL)) == 0 || max == ULONG_MAX) {
-					printf("Error: invalid numeric argument (%s)\n", argv[3]);
-					print_error_message();
-					return EXIT_FAILURE;
-				}
+		//4-arg call, "count" numbers generated - [min,max]
+		if (argc == 4) {
+			if ((min = strtod(argv[2], NULL)) == 0 || min == ULONG_MAX) {
+				printf("Error: invalid numeric argument (%s)\n", argv[3]);
+				print_error_message();
+				return EXIT_FAILURE;
 			}
-
-			//5-arg call, "count" numbers generated - [min,max]
-			if (argc == 5) {
-				if ((min = strtod(argv[3], NULL)) == 0 || min == ULONG_MAX) {
-					printf("Error: invalid numeric argument (%s)\n", argv[3]);
-					print_error_message();
-					return EXIT_FAILURE;
-				}
-				if ((max = strtod(argv[4], NULL)) == 0 || max == ULONG_MAX) {
-					printf("Error: invalid numeric argument (%s)\n", argv[4]);
-					print_error_message();
-					return EXIT_FAILURE;
-				}
+			if ((max = strtod(argv[3], NULL)) == 0 || max == ULONG_MAX) {
+				printf("Error: invalid numeric argument (%s)\n", argv[4]);
+				print_error_message();
+				return EXIT_FAILURE;
 			}
 		}
-		printf("Random Number Generator - Aaron Rader 2022\n\n");
-	}
 
-	if (argc == 1) { //no command-line arguments
-		printf(
-			"Random Number Generator - Aaron Rader 2022\n\n"
-			"Enter a positive integer denoting the amount of numbers you would like to generate: "
-		);
-		if (scanf("%ju", &count) != 1) {
-			printf("\nError: number is invalid.\n");
-			return EXIT_FAILURE;
-		}
-		printf("Enter min: ");
-		if (scanf("%lf", &min) != 1) {
-			printf("\nError: number is invalid.\n");
-			return EXIT_FAILURE;
-		}
-		printf("Enter max: ");
-		if (scanf("%lf", &max) != 1) {
-			printf("\nError: number is invalid.\n");
-			return EXIT_FAILURE;
+		if (argc == 5) {
+			stream = fopen(argv[4], "w");
+			if (stream == NULL) {
+				printf("Error: cannot open \"%s\": ", argv[1]);
+				perror("");
+				return EXIT_FAILURE;
+			}
 		}
 	}
 
-	printf("Generating random numbers...\n");
 	srand(time(NULL));
 	for (uintmax_t i = 0; i < count; ++i) {
 		fprintf(stream, "%lf ", (RAND_FLOAT * (max - min)) + min);
 	}
-	printf("\nSuccess!\n");
 	fclose(stream);
 }
