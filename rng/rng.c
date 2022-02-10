48f1578f2cf4682ff1305f1c0fad2c53fe9ecf51
aaronrader <aaronrader1867+github@gmail.com>
2022-02-09 23:04:27 -0500

    Removed file argument. Added constraints on input.

--- rng/rng.c
+++ rng/rng.c
@@ -10,16 +10,15 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <time.h>
+#include <string.h>
 
 #define RAND_FLOAT ((double)rand()/(double)RAND_MAX)
 
 void print_error_message() {
 	printf(
-		"Usage: fcount [filename] [(unsigned)numbers to generate] [min] [max]\n"
-		"\twhere filename is the name of a file.\n"
+		"Usage: rng.exe [numbers to generate] [min] [max]\n"
 		"\twhere numbers to generate is a positive integer.\n"
-		"\twhere max and min are real numbers.\n"
-		"\tmin is 0 by default, explicitly defining a 0 min will result in an error.\n"
+		"\twhere min and max are real numbers.\n"
 	);
 }
 
@@ -29,64 +28,64 @@ void print_error_message() {
 * 2. (unsigned)numbers to generate	(optional)
 * 3. minimum number to generate		(optional)
 * 4. maximum number to generate		(optional)
-* 5. file to output to				(optionsl)
 */
 int main(int argc, char* argv[]) {
-	if (argc > 5) {
+	if (argc > 4) {
 		printf("Error: too many command-line arguments (%d)\n", argc);
 		print_error_message();
 		return EXIT_FAILURE;
 	}
 
 	uintmax_t count = 1000;
+	intmax_t temp;
 	double max = 1;
 	double min = 0;
-	FILE* stream = stdout;
 
-	//Numeric argument defined
 	if (argc > 1) {
-		if ((count = strtoull(argv[1], NULL, 0)) == 0 || count == ULONG_MAX) {
+		if ((temp = strtoll(argv[1], NULL, 0)) < 0) {
+			printf("Error: invalid numeric argument (%s)\n", argv[1]);
+			print_error_message();
+			return EXIT_FAILURE;
+		}
+		count = strtoull(argv[1], NULL, 0);
+	}
+
+	//3-arg call, "count" numbers generated - [0,max]
+	if (argc == 3) {
+		if ((max = strtod(argv[2], NULL)) == 0 && strcmp(argv[2], "0") != 0) {
 			printf("Error: invalid numeric argument (%s)\n", argv[2]);
 			print_error_message();
 			return EXIT_FAILURE;
 		}
-
-		//3-arg call, "count" numbers generated - [0,max]
-		if (argc == 3) {
-			if ((max = strtod(argv[2], NULL)) == 0 || max == ULONG_MAX) {
-				printf("Error: invalid numeric argument (%s)\n", argv[3]);
-				print_error_message();
-				return EXIT_FAILURE;
-			}
+		if (max < 0) {
+			printf("Error: invalid numeric argument (%s): max should be greater than 0 when min is not defined\n", argv[2]);
+			print_error_message();
+			return EXIT_FAILURE;
 		}
+	}
 
-		//4-arg call, "count" numbers generated - [min,max]
-		if (argc == 4) {
-			if ((min = strtod(argv[2], NULL)) == 0 || min == ULONG_MAX) {
-				printf("Error: invalid numeric argument (%s)\n", argv[3]);
-				print_error_message();
-				return EXIT_FAILURE;
-			}
-			if ((max = strtod(argv[3], NULL)) == 0 || max == ULONG_MAX) {
-				printf("Error: invalid numeric argument (%s)\n", argv[4]);
-				print_error_message();
-				return EXIT_FAILURE;
-			}
+	//4-arg call, "count" numbers generated - [min,max]
+	if (argc == 4) {
+		if ((min = strtod(argv[2], NULL)) == 0 && strcmp(argv[2], "0") != 0) {
+			printf("Error: invalid numeric argument (%s)\n", argv[2]);
+			print_error_message();
+			return EXIT_FAILURE;
 		}
-
-		if (argc == 5) {
-			stream = fopen(argv[4], "w");
-			if (stream == NULL) {
-				printf("Error: cannot open \"%s\": ", argv[1]);
-				perror("");
-				return EXIT_FAILURE;
-			}
+		if ((max = strtod(argv[3], NULL)) == 0 && strcmp(argv[3], "0") != 0) {
+			printf("Error: invalid numeric argument (%s)\n", argv[3]);
+			print_error_message();
+			return EXIT_FAILURE;
+		}
+		if (max < min) {
+			printf("Error: invalid numeric argument (%s): max should be greater than min\n", argv[3]);
+			print_error_message();
+			return EXIT_FAILURE;
 		}
 	}
 
 	srand(time(NULL));
+	//printf("Count:\t%ju\nMin:\t%lf\nMax:\t%lf\n", count, min, max);
 	for (uintmax_t i = 0; i < count; ++i) {
-		fprintf(stream, "%lf ", (RAND_FLOAT * (max - min)) + min);
+		printf("%lf ", (RAND_FLOAT * (max - min)) + min);
 	}
-	fclose(stream);
 }
